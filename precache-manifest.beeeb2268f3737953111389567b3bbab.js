2cfb0e6a31407cd7df4a99b811d1779e485737a7
Henry Han <henry.han@usask.ca>
2020-03-02 01:45:15 -0500

    Updates

---
+++ precache-manifest.beeeb2268f3737953111389567b3bbab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69ca87974636a14a7acabffe563d9add",
+    "url": "/js-comment/index.html"
+  },
+  {
+    "revision": "6bf98fb89ece493d3ddc",
+    "url": "/js-comment/static/css/main.e20c895b.chunk.css"
+  },
+  {
+    "revision": "9b5e472a0baebb6c99b1",
+    "url": "/js-comment/static/js/2.b87fd287.chunk.js"
+  },
+  {
+    "revision": "df899a2ce02f888fa051548cf07a0e09",
+    "url": "/js-comment/static/js/2.b87fd287.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bf98fb89ece493d3ddc",
+    "url": "/js-comment/static/js/main.7654fdc5.chunk.js"
+  },
+  {
+    "revision": "5ab98581983e1354122b",
+    "url": "/js-comment/static/js/runtime-main.232c0b17.js"
+  }
+]);
